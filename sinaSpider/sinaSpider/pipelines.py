36afdb88f84a82453bf03070ed049599386a83c0
hyhlinux <2285020853@qq.com>
2016-12-10 11:18:37 +0800

    [sinaSpider][Modular][function]
    plan:test ok
    Type:
    Test:

--- sinaSpider/sinaSpider/pipelines.py
+++ sinaSpider/sinaSpider/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sys
+from scrapy import signals
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 class SinaspiderPipeline(object):
     def process_item(self, item, spider):
+        sonUrls = item['sonUrls']
+
+        # 文件名为子链接url中间部分，并将 / 替换为 _，保存为 .txt格式
+        filename = sonUrls[7:-6].replace('/','_')
+        filename += ".txt"
+
+        with open(item['subFilename']+'/'+filename, 'w') as fp:
+            fp.write(item['content'])
+
         return item
--- sinaSpider/sinaSpider/settings.py
+++ sinaSpider/sinaSpider/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'sinaSpider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'sinaSpider (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'sinaSpider.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'sinaSpider.pipelines.SinaspiderPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +88,4 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+LOG_LEVEL = 'DEBUG'
